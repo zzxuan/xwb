982243bcda8046de6ceeab74d662699ce1827bac
zzx_xuan <xiaoxuanfengzzx@163.com>
2015-10-07 22:27:45 +0800

    编码修改

--- reginputMobile.php
+++ reginputMobile.php
@@ -1,6 +1,6 @@
 
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=gb2312" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0" />
 <meta content="yes" name="apple-mobile-web-app-capable" />
 <meta content="black" name="apple-mobile-web-app-status-bar-style"  />
